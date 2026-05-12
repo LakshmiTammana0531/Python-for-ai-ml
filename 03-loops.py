8ae451d92396536303ad16f92ac053d86b0413b4
Lakshmi Sulochana Tammana <tchaithanyaharsha2019@gmail.com>
2026-05-12 23:38:05 +0530

    Update 03-loops.py

--- 03-loops.py
+++ 03-loops.py
@@ -1,90 +1,90 @@
-###loops are two types for loop and while loop
-##syntax:
-    # for i in range(start,stop,step): 
-    #     statements
+##loops are two types for loop and while loop
+#syntax:
+    for i in range(start,stop,step): 
+        statements
 
 
-########### for
-# for i in range(1,11):
-#     print(i)
+########## for
+for i in range(1,11):
+    print(i)
 
-# for i in range (1,21):
-#     if i%2 != 0:
-#         print(i)
+for i in range (1,21):
+    if i%2 != 0:
+        print(i)
 
-# # n= int(input("enter a num to print the string:"))
-# for i in range (a):
-#     print("lakshmi")
+# n= int(input("enter a num to print the string:"))
+for i in range (a):
+    print("lakshmi")
 
-# n= int(input("enter a num :"))
-# sum = 0
-# for i in range(1,n+1):
-#     sum=sum+i
-# print(sum) 
-
-
-# for i in range(1,11):
-#     print("5 x" ,(i), "=" ,(5*i)) 
-
-# result= int(input("enter a num to print factorial:"))
-# for i in range(1,result):
-#     result = result*i
-# print(result)
-
-# num= input("enter a num to know number of digits:")
-# count = 0
-# for i in num:
-#     count += 1
-# print(count)
-
-# num = int(input("enter a num to know number of digits: "))
-# b = str(num)
-# rev = ""
-# for i in b:
-#     rev = i + rev   
-# print(rev)
-
-# for i in range(1,10):
-#     if i==5:
-#         continue
-#     print(i)
-
-# for i in range(1,10):
-#     if i==5:
-#         break
-#     print(i)
-
-# # n = int(input("enter the  nth natural numbers:"))
-# sum=0
-# for i in range(1,n+1,2):
-#     sum=sum+i
-# print(sum)
-
-# n = 4
-# for i in range(1, n+1):
-#     print((str(i)+ ' ')*i)
-
-# for i in range(101,0,-1):
-#     if i%2==0:
-#         print(i)
+n= int(input("enter a num :"))
+sum = 0
+for i in range(1,n+1):
+    sum=sum+i
+print(sum) 
 
 
+for i in range(1,11):
+    print("5 x" ,(i), "=" ,(5*i)) 
 
-#########while
-####syntax
+result= int(input("enter a num to print factorial:"))
+for i in range(1,result):
+    result = result*i
+print(result)
+
+num= input("enter a num to know number of digits:")
+count = 0
+for i in num:
+    count += 1
+print(count)
+
+num = int(input("enter a num to know number of digits: "))
+b = str(num)
+rev = ""
+for i in b:
+    rev = i + rev   
+print(rev)
+
+for i in range(1,10):
+    if i==5:
+        continue
+    print(i)
+
+for i in range(1,10):
+    if i==5:
+        break
+    print(i)
+
+# n = int(input("enter the  nth natural numbers:"))
+sum=0
+for i in range(1,n+1,2):
+    sum=sum+i
+print(sum)
+
+n = 4
+for i in range(1, n+1):
+    print((str(i)+ ' ')*i)
+
+for i in range(101,0,-1):
+    if i%2==0:
+        print(i)
+
+
+
+########while
+###syntax
  
-    #  while condition:
-    #  increment
-    # statements
+     while condition:
+     increment
+    statements
 
 
-# i=100
-# while i>0:
-#     if i%2==0: 
-#         print(i)
-#     i=i-1  
+i=100
+while i>0:
+    if i%2==0: 
+        print(i)
+    i=i-1  
 
-# i=0
-# while i<5:
-#     i=i+1
-#     print(i)
+i=0
+while i<5:
+    i=i+1
+    print(i)
